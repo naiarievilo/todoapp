aec6cfb387f1b3ebe434d258690df72d19a24289
Ian Oliveira <naiarievilo@gmail.com>
2024-04-19 19:26:46 -0300

    Add `findByUsername` query to UserRepository

--- src/main/java/dev/naiarievilo/todoapp/users/UserRepository.java
+++ src/main/java/dev/naiarievilo/todoapp/users/UserRepository.java
@@ -2,5 +2,9 @@ package dev.naiarievilo.todoapp.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 }
