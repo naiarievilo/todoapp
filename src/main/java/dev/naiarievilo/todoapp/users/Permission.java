3830a2d5cbab2ce690358a4ea017a2a142d78239
Ian Oliveira <naiarievilo@gmail.com>
2024-04-30 22:33:35 -0300

    Add hashCode(), equals(), bidirectional relationship, and sync functions for `Permission` entity

--- src/main/java/dev/naiarievilo/todoapp/users/Permission.java
+++ src/main/java/dev/naiarievilo/todoapp/users/Permission.java
@@ -1,7 +1,12 @@
 package dev.naiarievilo.todoapp.users;
 
 import jakarta.persistence.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.hibernate.annotations.NaturalId;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "permissions")
@@ -15,6 +20,17 @@ public class Permission {
     @NaturalId
     @Column(name = "permission", unique = true, nullable = false, updatable = false)
     private String permission;
+
+    @ManyToMany(mappedBy = "permissions")
+    private Set<Role> roles = new LinkedHashSet<>();
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 
     public Long getId() {
         return id;
@@ -30,6 +46,28 @@ public class Permission {
 
     public void setPermission(String permission) {
         this.permission = permission;
+    }
+
+    @Override
+    public int hashCode() {
+        HashCodeBuilder hcb = new HashCodeBuilder();
+        hcb.append(permission);
+        return hcb.toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Permission other)) {
+            return false;
+        }
+
+        EqualsBuilder eb = new EqualsBuilder();
+        eb.append(permission, other.permission);
+        return eb.isEquals();
     }
 
 }
