bad8bb879885b3f10e2851d8ed21f371506ebae7
Ian Oliveira <naiarievilo@gmail.com>
2024-05-02 21:35:34 -0300

    Add `deleteByUsername` query method in `UserRepository`

--- src/main/java/dev/naiarievilo/todoapp/users/UserRepository.java
+++ src/main/java/dev/naiarievilo/todoapp/users/UserRepository.java
@@ -1,6 +1,7 @@
 package dev.naiarievilo.todoapp.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -9,4 +10,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    @Modifying
+    @Transactional
+    void deleteByUsername(String username);
 }
