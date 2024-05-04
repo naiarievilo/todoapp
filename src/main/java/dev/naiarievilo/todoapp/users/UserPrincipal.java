e6ca797594ca7e2069efa166ee22f4cf598803dc
Ian Oliveira <naiarievilo@gmail.com>
2024-05-04 02:10:39 -0300

    Implement `UserPrincipal` interface that extends `UserDetails` for additional methods

--- src/main/java/dev/naiarievilo/todoapp/users/UserPrincipal.java
+++ src/main/java/dev/naiarievilo/todoapp/users/UserPrincipal.java
@@ -1,71 +1,14 @@
 package dev.naiarievilo.todoapp.users;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Collection;
 
-public class UserPrincipal implements UserDetails {
+public interface UserPrincipal extends UserDetails {
 
-    private final User user;
+    String getEmail();
 
-    public UserPrincipal(User user) {
-        this.user = user;
-    }
+    Collection<? extends GrantedAuthority> getRoles();
 
-    public String getEmail() {
-        return user.getEmail();
-    }
-
-    public Collection<? extends GrantedAuthority> getRoles() {
-        Set<Role> roles = user.getRoles();
-
-        return roles.stream()
-            .map(Role::getRole)
-            .map(SimpleGrantedAuthority::new)
-            .collect(Collectors.toCollection(LinkedHashSet::new));
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Set<Role> roles = user.getRoles();
-
-        return roles.stream()
-            .flatMap(role -> role.getPermissions().stream())
-            .map(Permission::getPermission)
-            .map(SimpleGrantedAuthority::new)
-            .collect(Collectors.toCollection(LinkedHashSet::new));
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getUsername();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return !user.getIsExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return !user.getIsLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return user.getIsEnabled();
-    }
 }
