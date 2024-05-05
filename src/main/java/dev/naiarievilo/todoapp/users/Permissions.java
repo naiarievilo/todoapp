5110d0ffa0cbdbf5870c192f93917e9fa65ad9e6
Ian Oliveira <naiarievilo@gmail.com>
2024-05-04 23:02:51 -0300

    Add `getPermissions` and `hasPermissions` static methods to `Permissions` enum

--- src/main/java/dev/naiarievilo/todoapp/users/Permissions.java
+++ src/main/java/dev/naiarievilo/todoapp/users/Permissions.java
@@ -1,7 +1,20 @@
 package dev.naiarievilo.todoapp.users;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Permissions {
     READ,
     WRITE,
-    DELETE
+    DELETE;
+
+    static final List<String> permissions = Arrays.stream(Permissions.values()).map(Permissions::name).toList();
+
+    static List<String> getPermissions() {
+        return permissions;
+    }
+
+    static boolean hasPermission(String permission) {
+        return permissions.contains(permission);
+    }
 }
