48ab533c4f073e9294ff117ef3a122569fe22f63
Ian Oliveira <naiarievilo@gmail.com>
2024-05-04 23:02:34 -0300

    Add `getRoles` and `hasRole` static methods to `Roles` enum

--- src/main/java/dev/naiarievilo/todoapp/users/Roles.java
+++ src/main/java/dev/naiarievilo/todoapp/users/Roles.java
@@ -1,6 +1,20 @@
 package dev.naiarievilo.todoapp.users;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Roles {
     ROLE_ADMIN,
-    ROLE_USER
+    ROLE_USER;
+
+    static final List<String> roles = Arrays.stream(Roles.values()).map(Roles::name).toList();
+
+    static List<String> getRoles() {
+        return roles;
+    }
+
+    static boolean hasRole(String role) {
+        return roles.contains(role);
+    }
+
 }
