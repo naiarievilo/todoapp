338ddadda48b18a2344519927fa4c8d30c77d7aa
Ian Oliveira <naiarievilo@gmail.com>
2024-05-06 16:25:19 -0300

    Update `UserPrincipalImpl`

--- src/main/java/dev/naiarievilo/todoapp/users/UserPrincipalImpl.java
+++ src/main/java/dev/naiarievilo/todoapp/users/UserPrincipalImpl.java
@@ -2,8 +2,6 @@ package dev.naiarievilo.todoapp.users;
 
 import dev.naiarievilo.todoapp.permissions.Permissions;
 import dev.naiarievilo.todoapp.roles.Roles;
-import dev.naiarievilo.todoapp.users.User;
-import dev.naiarievilo.todoapp.users.UserServiceImpl;
 import org.apache.commons.lang3.Validate;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,45 +10,40 @@ import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import static dev.naiarievilo.todoapp.validation.ValidationMessages.*;
+
 public class UserPrincipalImpl implements UserPrincipal {
 
     private final String username;
     private final String password;
     private final String email;
-    private final Set<GrantedAuthority> authorities;
     private final Set<GrantedAuthority> roles;
+    private final Set<GrantedAuthority> permissions;
     private final boolean isExpired;
     private final boolean isLocked;
     private final boolean isEnabled;
 
-    public UserPrincipalImpl(String username, String password, String email, Set<GrantedAuthority> roles,
-        Set<GrantedAuthority> authorities) {
-        this(username, password, email, roles, authorities, false, false, true);
-    }
-
     private UserPrincipalImpl(String username, String password, String email, Set<GrantedAuthority> roles,
-        Set<GrantedAuthority> authorities, boolean isExpired, boolean isLocked, boolean isEnabled) {
+        Set<GrantedAuthority> permissions, boolean isExpired, boolean isLocked, boolean isEnabled) {
 
-        Validate.notBlank(username, "Username must not be blank");
-        Validate.notBlank(password, "Password must not be blank");
-        Validate.notBlank(email, "Email must not be blank");
-        Validate.notEmpty(roles, "Roles must not be empty");
-        Validate.noNullElements(roles, "Roles must not contain null elements");
-        Validate.notEmpty(authorities, "Authorities must not be empty");
-        Validate.noNullElements(authorities, "Authorities must not contain null elements");
+        Validate.notBlank(username, USERNAME_NOT_BLANK.message());
+        Validate.notBlank(password, PASSWORD_NOT_BLANK.message());
+        Validate.notBlank(email, EMAIL_NOT_BLANK.message());
+        Validate.noNullElements(roles, ROLES_NOT_NULL_OR_EMPTY.message());
+        Validate.noNullElements(permissions, PERMISSIONS_NOT_NULL_OR_EMPTY.message());
 
         this.username = username;
         this.password = password;
         this.email = email;
         this.roles = roles;
-        this.authorities = authorities;
+        this.permissions = permissions;
         this.isExpired = isExpired;
         this.isLocked = isLocked;
         this.isEnabled = isEnabled;
     }
 
     public static UserPrincipalImplBuilder withUsername(String username) {
-        Validate.notBlank(username, "Username must not be blank");
+        Validate.notBlank(username, USERNAME_NOT_BLANK.message());
 
         return builder().setUsername(username);
     }
@@ -60,14 +53,14 @@ public class UserPrincipalImpl implements UserPrincipal {
     }
 
     public static UserPrincipal withUser(User user) {
-        Validate.notNull(user, "User must not be null");
+        Validate.notNull(user, USER_NOT_NULL.message());
 
         return builder()
             .setUsername(user.getUsername())
             .setPassword(user.getPassword())
             .setEmail(user.getEmail())
             .setRoles(UserServiceImpl.getUserRoles(user))
-            .setAuthorities(UserServiceImpl.getUserAuthorities(user))
+            .setPermissions(UserServiceImpl.getUserPermissions(user))
             .setExpired(user.getIsExpired())
             .setLocked(user.getIsLocked())
             .setEnabled(user.getIsEnabled())
@@ -86,7 +79,7 @@ public class UserPrincipalImpl implements UserPrincipal {
 
     @Override
     public Collection<GrantedAuthority> getAuthorities() {
-        return authorities;
+        return permissions;
     }
 
     @Override
@@ -122,7 +115,7 @@ public class UserPrincipalImpl implements UserPrincipal {
     public static final class UserPrincipalImplBuilder {
 
         private final Set<GrantedAuthority> roles = new LinkedHashSet<>();
-        private final Set<GrantedAuthority> authorities = new LinkedHashSet<>();
+        private final Set<GrantedAuthority> permissions = new LinkedHashSet<>();
         private String username;
         private String password;
         private String email;
@@ -131,29 +124,28 @@ public class UserPrincipalImpl implements UserPrincipal {
         private boolean isEnabled;
 
         public UserPrincipalImplBuilder setUsername(String username) {
-            Validate.notBlank(username, "Username must not be blank");
+            Validate.notBlank(username, USERNAME_NOT_BLANK.message());
 
             this.username = username;
             return this;
         }
 
         public UserPrincipalImplBuilder setPassword(String password) {
-            Validate.notBlank(password, "Password must not be blank");
+            Validate.notBlank(password, PASSWORD_NOT_BLANK.message());
 
             this.password = password;
             return this;
         }
 
         public UserPrincipalImplBuilder setEmail(String email) {
-            Validate.notBlank(email, "Email must not be blank");
+            Validate.notBlank(email, EMAIL_NOT_BLANK.message());
 
             this.email = email;
             return this;
         }
 
         public UserPrincipalImplBuilder setRoles(Roles... roles) {
-            Validate.notEmpty(roles, "Must provide at least one role");
-            Validate.noNullElements(roles, "Roles must not contain null elements");
+            Validate.noNullElements(roles, ROLES_NOT_NULL_OR_EMPTY.message());
 
             for (Roles role : roles) {
                 this.roles.add(new SimpleGrantedAuthority(role.name()));
@@ -163,29 +155,26 @@ public class UserPrincipalImpl implements UserPrincipal {
         }
 
         public UserPrincipalImplBuilder setRoles(Set<GrantedAuthority> roles) {
-            Validate.notEmpty(roles, "Must provide at least one role");
-            Validate.noNullElements(roles, "Roles must not contain null elements");
+            Validate.noNullElements(roles, ROLES_NOT_NULL_OR_EMPTY.message());
 
             this.roles.addAll(roles);
             return this;
         }
 
-        public UserPrincipalImplBuilder setAuthorities(Permissions... permissions) {
-            Validate.notEmpty(permissions, "Must provide at least one permission");
-            Validate.noNullElements(permissions, "Permissions must not contain null elements");
+        public UserPrincipalImplBuilder setPermissions(Permissions... permissions) {
+            Validate.noNullElements(permissions, PERMISSIONS_NOT_NULL_OR_EMPTY.message());
 
             for (Permissions permission : permissions) {
-                this.authorities.add(new SimpleGrantedAuthority(permission.name()));
+                this.permissions.add(new SimpleGrantedAuthority(permission.name()));
             }
 
             return this;
         }
 
-        public UserPrincipalImplBuilder setAuthorities(Set<GrantedAuthority> permissions) {
-            Validate.notEmpty(permissions, "Must provide at least one permission");
-            Validate.noNullElements(permissions, "Permissions must not contain null elements");
+        public UserPrincipalImplBuilder setPermissions(Set<GrantedAuthority> permissions) {
+            Validate.noNullElements(permissions, PERMISSIONS_NOT_NULL_OR_EMPTY.message());
 
-            this.authorities.addAll(permissions);
+            this.permissions.addAll(permissions);
             return this;
         }
 
@@ -206,7 +195,7 @@ public class UserPrincipalImpl implements UserPrincipal {
         }
 
         public UserPrincipal build() {
-            return new UserPrincipalImpl(username, password, email, roles, authorities, isExpired, isLocked, isEnabled);
+            return new UserPrincipalImpl(username, password, email, roles, permissions, isExpired, isLocked, isEnabled);
         }
 
     }
