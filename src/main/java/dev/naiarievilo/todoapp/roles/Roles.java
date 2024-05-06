95cfebe681ee82580b3e20105f0664a387fac81e
Ian Oliveira <naiarievilo@gmail.com>
2024-05-05 21:25:14 -0300

    Update `Roles` enum
    Add constructor, `roleNames` and `description` private fields, and static methods

---
+++ src/main/java/dev/naiarievilo/todoapp/roles/Roles.java
@@ -0,0 +1,29 @@
+package dev.naiarievilo.todoapp.roles;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Roles {
+    ROLE_ADMIN("Superuser role"),
+    ROLE_USER("Default account role");
+
+    static final List<String> roleNames = Arrays.stream(Roles.values()).map(Roles::name).toList();
+    private final String description;
+
+    Roles(String description) {
+        this.description = description;
+    }
+
+    static List<String> getRoles() {
+        return roleNames;
+    }
+
+    static boolean hasRole(String role) {
+        return roleNames.contains(role);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
