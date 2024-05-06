68d8f62f4e95ec4d7189573675ed47bceb75b046
Ian Oliveira <naiarievilo@gmail.com>
2024-05-06 16:24:41 -0300

    Define `UserService` interface

--- src/main/java/dev/naiarievilo/todoapp/users/UserService.java
+++ src/main/java/dev/naiarievilo/todoapp/users/UserService.java
@@ -1,35 +1,20 @@
 package dev.naiarievilo.todoapp.users;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.provisioning.GroupManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-public interface UserService extends UserDetailsManager, GroupManager {
-
-    static Set<GrantedAuthority> getUserAuthorities(User user) {
-        return user.getRoles().stream()
-            .flatMap(role -> role.getPermissions().stream())
-            .map(Permission::getPermission)
-            .map(SimpleGrantedAuthority::new)
-            .collect(Collectors.toCollection(LinkedHashSet::new));
-    }
-
-    static Set<GrantedAuthority> getUserRoles(User user) {
-        return user.getRoles().stream()
-            .map(Role::getRole)
-            .map(SimpleGrantedAuthority::new)
-            .collect(Collectors.toCollection(LinkedHashSet::new));
-    }
+public interface UserService extends UserDetailsService {
 
     @Override
     UserPrincipal loadUserByUsername(String username);
 
     void createUser(UserPrincipal user);
 
-    void updateUser(UserPrincipal user);
+    void updateUser(User user);
+
+    void deleteUser(String username);
+
+    void changePassword(String oldPassword, String newPassword);
+
+    boolean userExists(String username);
+
 }
