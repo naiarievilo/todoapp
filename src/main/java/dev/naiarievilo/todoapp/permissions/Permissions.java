69e638fd1db207a0ebb28dbdc08658597027a5d0
Ian Oliveira <naiarievilo@gmail.com>
2024-05-05 23:09:59 -0300

    Update `Permissions` enum

---
+++ src/main/java/dev/naiarievilo/todoapp/permissions/Permissions.java
@@ -0,0 +1,29 @@
+package dev.naiarievilo.todoapp.permissions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Permissions {
+    READ("Can access resources"),
+    WRITE("Can create or modify resources"),
+    DELETE("Can delete resources");
+
+    static final List<String> permissionNames = Arrays.stream(Permissions.values()).map(Permissions::name).toList();
+    private final String description;
+
+    Permissions(String description) {
+        this.description = description;
+    }
+
+    static List<String> getPermissionNames() {
+        return permissionNames;
+    }
+
+    static boolean hasPermission(String permission) {
+        return permissionNames.contains(permission);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
