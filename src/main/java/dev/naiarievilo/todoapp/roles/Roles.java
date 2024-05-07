e07d592d089e8502e5e0476ec7e66b2b15b76a76
Ian Oliveira <naiarievilo@gmail.com>
2024-05-06 21:14:57 -0300

    Update `Roles` methods and fields
    Add `getRole(String role)` and change `List<String> roleNames` to `Set<Roles> rolesSet`

--- src/main/java/dev/naiarievilo/todoapp/roles/Roles.java
+++ src/main/java/dev/naiarievilo/todoapp/roles/Roles.java
@@ -1,25 +1,53 @@
 package dev.naiarievilo.todoapp.roles;
 
+import org.apache.commons.lang3.Validate;
+
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static dev.naiarievilo.todoapp.validation.ValidationMessages.NOT_BLANK;
 
 public enum Roles {
     ROLE_ADMIN("Superuser role"),
     ROLE_USER("Default account role");
 
-    static final List<String> roleNames = Arrays.stream(Roles.values()).map(Roles::name).toList();
+    static final Set<Roles> rolesSet =
+        Arrays.stream(Roles.values()).collect(Collectors.toCollection(LinkedHashSet::new));
     private final String description;
 
     Roles(String description) {
         this.description = description;
     }
 
-    public static List<String> roleNames() {
-        return roleNames;
+    public static Optional<Roles> getRole(String role) {
+        Validate.notBlank(role, NOT_BLANK.message());
+
+        for (Roles roles : rolesSet) {
+            if (roles.name().equals(role)) {
+                return Optional.of(roles);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public static Set<Roles> roles() {
+        return rolesSet;
     }
 
     public static boolean hasRole(String role) {
-        return roleNames.contains(role);
+        Validate.notNull(role, NOT_BLANK.message());
+
+        for (Roles roles : rolesSet) {
+            if (roles.name().equals(role)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public String description() {
