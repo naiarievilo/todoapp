3c958d141ac535f0ecc5fed77bc5a788180b8d83
Ian Oliveira <naiarievilo@gmail.com>
2024-05-17 17:02:18 -0300

    Implement password-matching validation

---
+++ src/main/java/dev/naiarievilo/todoapp/validation/PasswordMatching.java
@@ -0,0 +1,25 @@
+package dev.naiarievilo.todoapp.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = PasswordMatchingValidator.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PasswordMatching {
+
+    String password();
+
+    String confirmPassword();
+
+    String message() default "Passwords provided must match";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
