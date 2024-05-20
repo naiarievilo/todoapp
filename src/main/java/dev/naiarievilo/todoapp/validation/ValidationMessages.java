46195f3360ee5f53023d776873fb766edceb9c11
Ian Oliveira <naiarievilo@gmail.com>
2024-05-19 21:48:38 -0300

    Update `ValidationMessages` enum
    Add `IS_INSTANCE_OF` constant and both `message(String target, String className)` and `message(String parameter)` for generating descriptive message.

--- src/main/java/dev/naiarievilo/todoapp/validation/ValidationMessages.java
+++ src/main/java/dev/naiarievilo/todoapp/validation/ValidationMessages.java
@@ -1,10 +1,13 @@
 package dev.naiarievilo.todoapp.validation;
 
+import org.apache.commons.lang3.Validate;
+
 public enum ValidationMessages {
-    NOT_BLANK("Parameter must not be blank"),
-    NOT_EMPTY("Parameter must not be empty"),
-    NOT_NULL("Parameter must not be null"),
-    NO_NULL_ELEMENTS("Parameter must not be null or contain null elements");
+    IS_INSTANCE_OF("%s must be an instance of %s"),
+    NOT_BLANK("%s must not be blank"),
+    NOT_EMPTY("%s must not be empty"),
+    NOT_NULL("%s must not be null"),
+    NO_NULL_ELEMENTS("%s must not be null or contain null elements");
 
     private final String message;
 
@@ -12,7 +15,19 @@ public enum ValidationMessages {
         this.message = message;
     }
 
+    public String message(String target, String className) {
+        Validate.notBlank(target, NOT_BLANK.message("target"));
+        Validate.notBlank(className, NOT_BLANK.message("className"));
+
+        return String.format(message, target, className);
+    }
+
+    public String message(String parameter) {
+        Validate.notBlank(parameter, NOT_BLANK.message());
+        return String.format(message, parameter);
+    }
+
     public String message() {
-        return message;
+        return String.format(message, "Parameter");
     }
 }
